91234d9e213c88b5c2c3a0fa099d51d3531d1071
Marco <marco.chianese93@gmail.com>
2020-12-02 11:18:38 +0100

    Aggiunta dinamica liste header e footer

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#root',
   data: {
-    message: 'Hello Vue!'
-  }
+    message: 'Hello Vue!' ,
+    headerList : ['courses','zoom','pages','bundles','course formats','demos'],
+    footerList: ['blog','home','shortcodes','courses','membership','typografy']
+    },
+    mounted () {
+        console.log(this.headerList);
+    }
+
 })
